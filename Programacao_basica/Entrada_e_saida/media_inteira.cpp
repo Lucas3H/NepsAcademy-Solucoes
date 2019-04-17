b210c9297ea43626cadfbd572907d3e7089a7c15
Lucas3H <45634893+Lucas3H@users.noreply.github.com>
2019-04-17 13:57:16 -0300

    Create media_inteira.cpp

---
+++ Programacao_basica/Entrada_e_saida/media_inteira.cpp
@@ -0,0 +1,15 @@
+Link: https://neps.academy/lesson/122
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, b;
+    
+   	cin >> a >> b;
+    
+    cout << (a+b)/2 << endl;
+    
+    return 0;
+}
