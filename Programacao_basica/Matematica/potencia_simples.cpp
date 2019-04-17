0c26e5f019995ba1ac4466fd1970b01fad9ff785
Lucas3H <45634893+Lucas3H@users.noreply.github.com>
2019-04-17 14:23:49 -0300

    Create potencia_simples.cpp

---
+++ Programacao_basica/Matematica/potencia_simples.cpp
@@ -0,0 +1,18 @@
+// https://neps.academy/lesson/137
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+	double x, y;
+	cin >> x >> y;
+	
+	cout.precision(4);
+	cout.setf(ios::fixed);
+	
+  cout << pow(x,y);
+  
+  return 0;
+}
