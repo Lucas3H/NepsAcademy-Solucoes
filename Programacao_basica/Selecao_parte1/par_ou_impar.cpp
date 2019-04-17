eeba6c030e7068b3e914e53f5daca77a13c89e59
Lucas3H <45634893+Lucas3H@users.noreply.github.com>
2019-04-17 14:04:55 -0300

    Create par_ou_impar.cpp

---
+++ Programacao_basica/Selecao_parte1/par_ou_impar.cpp
@@ -0,0 +1,21 @@
+//Link: https://neps.academy/lesson/127
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a, b;
+    
+    cin >> a;
+    cin >> b;
+    
+    if((a+b)%2 == 0){
+    	cout << "Bino" << endl;
+    }
+    
+    else{
+    	cout << "Cino" << endl;
+    }
+}
