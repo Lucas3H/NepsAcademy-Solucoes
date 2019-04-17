678e695428857b7e3406b7562576f17a7fd46e3f
Lucas3H <45634893+Lucas3H@users.noreply.github.com>
2019-04-17 14:01:48 -0300

    Create bondinho.cpp

---
+++ Programacao_basica/Selecao_parte1/bondinho.cpp
@@ -0,0 +1,21 @@
+//Link: https://neps.academy/lesson/126
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, m;
+    
+    cin >> a >> m;
+    
+    if((a+m)>50){
+    	cout << "N" << endl;
+    }
+    
+   	else{
+    	cout << "S" << endl;
+    }
+    
+    return 0;
+}
