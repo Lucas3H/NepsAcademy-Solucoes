eb081d17fe3178b19f4b5280a64202df27811bee
Lucas3H <45634893+Lucas3H@users.noreply.github.com>
2019-04-17 14:09:38 -0300

    Create fliper.cpp

---
+++ Programacao_basica/Selecao_parte2/fliper.cpp
@@ -0,0 +1,22 @@
+//Link: https://neps.academy/lesson/128
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	bool p, r;
+	cin>>p>>r;
+  
+	if(p==0){
+		cout<<"C";
+	}else{
+		if(r==0){
+			cout<<"B";
+		}else{
+			cout<<"A";
+		}
+	}
+  
+  return 0;
+}
